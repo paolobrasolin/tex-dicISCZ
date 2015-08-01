e3a25d02d78f6125c286fcfb6d09c1fa081ac501
Paolo Brasolin <paolo.brasolin@gmail.com>
2015-08-01 12:04:20 +0200

    Fixing index, small tweaks

---
+++ README.md
@@ -0,0 +1,4 @@
+`iec*` files are from https://github.com/michal-h21/iec2utf
+
+`mal-*` files are from http://tex.stackexchange.com/a/170370/82186
+
--- make.sh
+++ make.sh
@@ -1,25 +1,15 @@
-#!/bin/bash
-#cd Dokumenty/hvalur.org/tmp/
-#vlna -l -m -n dictionary_uvod.tex
-#vlna -l -m -n dictionary_historie.tex
-#vlna -l -m -n dictionary_instructions.tex
-#cd letters/
-#vlna -l -m -n dictionary.tex
-#cd ..
-#pdflatex dict_main.tex
-#pdflatex dict_main.tex
-##texlua utftexindy.lua -L icelandic dict_main.idx
-##makeindex dict_main
-#texlua iec2utf.lua <dict_main.idx | xindy -C utf8 -L icelandic -M texindy -o dict_main.ind
-##xindy -M texindy -M icelandicmal-test -M mal-style  dict_main.idx
-##texindy -L icelandic -M lang/icelandic/utf8 dict_main.idx
-##xindy -M texindy -C utf8 -L icelandic dict_main.idx
-#pdflatex dict_main.tex
-
+# First run to generate the index file and mal/style
 pdflatex main
-texlua iec2utf.lua < main.idx > mal-temp.idx
-xindy -M texindy -M icelandicmalmin -M mal-style -o main.ind mal-temp.idx
+
+# We convert its encoding to UTF8 using github.com/michal-h21/iec2utf
+texlua iec2utf.lua < main.idx > main-utf8.idx
+# We produce the sorted index using the module by Malipivo
+xindy -M texindy -M mal-icelandic-min -M mal-style -o main.ind main-utf8.idx
+
+# Standard bibliography compilation
 biber main
+
+# Finishing runs
 pdflatex main
 pdflatex main
 
