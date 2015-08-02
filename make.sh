93a0469fa5ee99230967346b1d51ff62ca1114c3
Paolo Brasolin <paolo.brasolin@gmail.com>
2015-08-02 13:55:46 +0200

    working on grid

--- make.sh
+++ make.sh
@@ -1,15 +1,15 @@
 # First run to generate the index file and mal/style
-pdflatex main
+#pdflatex main
 
-# We convert its encoding to UTF8 using github.com/michal-h21/iec2utf
-texlua iec2utf.lua < main.idx > main-utf8.idx
-# We produce the sorted index using the module by Malipivo
-xindy -M texindy -M mal-icelandic-min -M mal-style -o main.ind main-utf8.idx
+# First we convert the *.idx encoding to UTF8
+texlua iec2utf.lua < figures.idx | \
+  xindy -M texindy -M mal-icelandic-min -M style -o figures.ind
+# then we pipe the result to xindy that sets it with our modules
 
 # Standard bibliography compilation
-biber main
+#biber main
 
 # Finishing runs
-pdflatex main
-pdflatex main
+#pdflatex main
+#pdflatex main
 
